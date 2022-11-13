b19e6e57163f740663d20902d245d84fd86dba94
solanacryptodev <eoscrypto87@protonmail.com>
2022-11-13 13:54:16 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+mod map;
+mod player;
+mod map_builder;
+mod camera;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub use legion::*;
+    pub use legion::world::SubWorld;
+    pub use legion::systems::CommandBuffer;
+    pub use crate::map::*;
+    pub use crate::player::*;
+    pub use crate::map_builder::*;
+    pub use crate::camera::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+}
+
+use prelude::*;
+use crate::map::Map;
+
+// enum GameMode {
+//     Menu,
+// }
+
+// Modeling a struct called State
+struct State {
+    map: Map,
+    player: Player,
+    camera: Camera
+}
+impl State {
+    // a constructor that initializes a new State instance and sets the default mode to Menu
+    fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        Self {
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start)
+        }
+    }
+}
+
+// implement a trait for the State struct
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
+        ctx.cls();
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
+    }
+}
+
+fn main() -> BError {
+
+    let context = BTermBuilder::new()
+        .with_fps_cap(30.0)
+        .with_title("Dragon Game")
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32,32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT,"dungeonfont.png")
+        .build()?;
+
+    main_loop(context, State::new())
+}
